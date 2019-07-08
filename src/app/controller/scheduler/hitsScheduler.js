512716efbc5b63679d93a1a083b2090309364931
SimJungUk <simju9397@gmail.com>
2019-07-09 02:10:05 +0900

    [적중곡 스케쥴러 수정]

--- src/app/controller/scheduler/hitsScheduler.js
+++ src/app/controller/scheduler/hitsScheduler.js
@@ -12,7 +12,9 @@ schedule.scheduleJob('0 5 0 1/1 * ? *', async function () {
     const updateHitSongCountQuery = 'UPDATE user SET hitSongCount=? WHERE userIdx = ?'
     const addHitPointQuery = 'INSERT INTO point_history(userIdx, songIdx, getPoint) VALUES(?, ?, ?)';
     const updateTotalPointQuery = 'UPDATE user SET totalPoint=? WHERE userIdx = ?';
+    const getPointHistoryQuery = 'SELECT songIdx FROM point_history WHERE userIdx = ?'
     const getAllUserDataResult = await pool.queryParam_None(getAllUserDataQuery);
+    let pointVerify=0;
     for (var i = 0; i < getAllUserDataResult.length; i++) {
         if (getAllUserDataResult[i].rateSongCount == 0) { //평가한 곡 없으면 넘어가고
             continue;
@@ -23,10 +25,23 @@ schedule.scheduleJob('0 5 0 1/1 * ? *', async function () {
             const hitsPlaylistIdx = (await playlistModules.getPlayList(userIdx, 'hits'))._id;
             const ratedSongList = await playlistModules.getSongList(ratedPlaylistIdx);
             const getUserRateScoreResult = await pool.queryParam_Arr(getUserRateScoreQuery, [userIdx]);
+            const getPointHistoryResult = await pool.queryParam_Arr(getPointHistoryQuery, [userIdx]);
+            console.log(getPointHistoryResult);
             for (var j = 0; j < ratedSongList.length; j++) {
                 for (var k = 0; k < getUserRateScoreResult.length; k++) {
-                    let songIdx =  ratedSongList[j]._id;
-                    if (songIdx == getUserRateScoreResult[k].songIdx) { //평가한 노래 리스트에 평가대기곡 리스트의 노래가 있을 때
+                    let songIdx = ratedSongList[j]._id;
+                    console.log('점수받은 목록 수 : ' + getPointHistoryResult.length);
+                    console.log('비교할 곡 : ' + songIdx);
+                    
+                    for (var l = 0; l < getPointHistoryResult.length; l++) {
+                        if (songIdx == getPointHistoryResult[l].songIdx) {
+                            console.log('내가 평가한 곡 : ' + getPointHistoryResult[l].songIdx);
+                            console.log('====================이미 점수 받음====================');
+                            pointVerify = 0;
+                            break;
+                        }
+                    }
+                    if (songIdx == getUserRateScoreResult[k].songIdx && pointVerify !== 0) { //평가한 노래 리스트에 평가대기곡 리스트의 노래가 있을 때
                         if (ratedSongList[j].songStatus == 1 || ratedSongList[j].songStatus == 2) { //유보상태가 아니면
                             let average = (parseFloat(ratedSongList[j].rateScore / ratedSongList[j].rateUserCount).toFixed(1));
                             if (Math.abs(average - getUserRateScoreResult[k].ratePoint) <= 0.3) { // 평균 점수-내점수 절댓값 0.3 이하
@@ -34,7 +49,7 @@ schedule.scheduleJob('0 5 0 1/1 * ? *', async function () {
                                 await playlistModules.addSongToPlaylist(hitsPlaylistIdx, ratedSongList[j]); //적중곡 목록에 추가
                                 let newCount = (getAllUserDataResult[i].hitSongCount + 1);
                                 await pool.queryParam_Arr(updateHitSongCountQuery, [newCount, userIdx]); // 적중곡수 수정
-                                await pool.queryParam_Arr(addHitPointQuery, [userIdx, songIdx.toString() ,400]); //포인트 내역 추가
+                                await pool.queryParam_Arr(addHitPointQuery, [userIdx, songIdx.toString(), 400]); //포인트 내역 추가
                                 let newPoint = (getAllUserDataResult[i].totalPoint + 400);
                                 await pool.queryParam_Arr(updateTotalPointQuery, [newPoint, userIdx]); //총 포인트 수정
                             }
@@ -42,6 +57,7 @@ schedule.scheduleJob('0 5 0 1/1 * ? *', async function () {
                     }
                 }
             }
+
         }
     }
 })
