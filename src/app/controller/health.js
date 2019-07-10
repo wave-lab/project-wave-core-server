c1969a39cf03e2fa61ecc0dbf2a6561e2eb237fa
SimJungUk <simju9397@gmail.com>
2019-07-11 00:02:05 +0900

    users

--- src/app/controller/health.js
+++ src/app/controller/health.js
@@ -4,7 +4,7 @@ const responseUtil = require('../module/responseUtil');
 const returnCode = require('../model/returnCode');
 
 router.get('/', async(req, res, next) => {
-    return res.status(200).send(responseUtil.successTrue(returnCode.OK, "Scheduler SERVER RUNNING..."));
+    return res.status(200).send(responseUtil.successTrue(returnCode.OK, "Core SERVER RUNNING..."));
 });
 
 module.exports = router; 
