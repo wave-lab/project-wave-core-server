1b3ed2d91ff5913b0809e71364a0e1c7caa67728
SimJungUk <simju9397@gmail.com>
2019-07-10 15:08:47 +0900

    수정1

--- src/app/controller/home/home_routes.js
+++ src/app/controller/home/home_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 
 // 마이페이지 조회, 수정
-router.use('/home/homeUserInfo', require('./homeUserInfo'));
+router.use('/UserInfo', require('./homeUserInfo'));
 
 router.use('/home/homePlaylist', require('./homePlaylist'));
 
