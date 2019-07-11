c662038fed7f6a39b298bef9df8f828f05e8376c
SimJungUk <simju9397@gmail.com>
2019-07-12 04:56:53 +0900

    이메일인증 post

--- src/app/controller/user/nicknameCheck.js
+++ src/app/controller/user/nicknameCheck.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../../module/pool.js');
-
+const qs = require('querystring');
 const returnCode = require('../../model/returnCode');
 const returnMessage = require('../../../config/returnMessage');
 const responseUtil = require('../../module/responseUtil');
@@ -9,7 +9,8 @@ const responseUtil = require('../../module/responseUtil');
 //nickname 중복 체크
 router.get('/', async (req, res, next) => {
     const selectNicknameQuery = 'SELECT nickname FROM user WHERE nickname = ?';
-    const selectNicknameResult = await pool.queryParam_Arr(selectNicknameQuery, [req.query.nickname]);
+    console.log(qs.unescape(req.query.nickname));
+    const selectNicknameResult = await pool.queryParam_Arr(selectNicknameQuery, [qs.unescape(req.query.nickname)]);
     
     if(selectNicknameResult[0] == null){ // 닉네임 중복이 없을 때
         res.status(200).send(responseUtil.successTrue(returnCode.OK, returnMessage.NICKNAME_CHECK_SUCCESS));
