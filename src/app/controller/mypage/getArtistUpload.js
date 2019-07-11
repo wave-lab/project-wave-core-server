2e281f0493fde809d1bf503c9b39c791dbae68aa
SimJungUk <simju9397@gmail.com>
2019-07-12 01:48:42 +0900

    사용자 곡 조회

--- src/app/controller/mypage/getArtistUpload.js
+++ src/app/controller/mypage/getArtistUpload.js
@@ -1,40 +1,30 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true })
-const pool = require('../../module/pool.js');
-const jwt = require('../../module/jwt');
-const playlistModules = require('../../module/playlistModules');
-const upload = require('../../../config/multer');
-const song = require('../../model/schema/song');
+const router = express.Router({ mergeParams: true });
 
 const returnCode = require('../../model/returnCode');
-const returnMessage = require('../../../config/returnMessage');
 const responseUtil = require('../../module/responseUtil');
-const history = require('../../model/schema/history');
+const playlist = require('../../model/schema/playlist');
+const myPlaylist = require('../../model/schema/myPlaylist');
 
-//아티스트 페이지 조회
+//아티스트가 업로드 한 곡 조회
+//해당 유저의 업로드 곡 조회 중에 pass한 곡, songStatus = 1
 router.get('/', async (req, res, next) => {
     const userIdx = req.params.userIdx;
 
-    const selectUserQuery = 'SELECT comment, nickname, profileImg, backImg FROM user WHERE userIdx = ?';
-    const selectUserResult = await pool.queryParam_Arr(selectUserQuery, userIdx);
+    const result = (await myPlaylist.find({ userIdx: userIdx }))[0];
 
-    if(selectUserResult.length != 0) {
-        const userInfo = selectUserResult[0]; // user의 모든 정보
-    
-        const allSong = await song.find({ userIdx: userIdx });
-        let count = 0;
-    
-        for (i = 0; i < allSong.length; i++) {
-            if (allSong[i].songStatus == 1) {
-                count += allSong[i].likeCount;
-            }
+    if (result != undefined) {
+
+        let query = {
+            '_id': result.uploadPlaylist,
+            'songList.songStatus': 1
         }
-    
-        userInfo.allSongLikeCount = count;
-    
-        res.status(200).send(responseUtil.successTrue(returnCode.OK, returnMessage.MYPAGE_SUCCESS, userInfo));
-    }else {
-        res.status(200).send(responseUtil.successFalse(returnCode.NOT_FOUND, "사용자를 찾을수 없습니다."));
+
+        const result2 = (await playlist.find(query))[0];
+
+        res.status(200).send(responseUtil.successTrue(returnCode.OK, "사용자 곡 조회", result2));
+    } else {
+        res.status(200).send(responseUtil.successFalse(returnCode.NOT_FOUND, "사용자를 찾을 수 없습니다."));
     }
 });
 
